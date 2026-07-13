e9b0fc93c497892f440f78c4004cf590afc40a73
magno v gomes <magnovgomes74@gmail.com>
2026-07-13 19:28:36 -0300

    feat: tipo unknown - alternativa segura ao any que exige verificação de tipo antes do uso

--- Secao19-TS-TiposBasicos/aula10-Tipo-unknown.ts
+++ Secao19-TS-TiposBasicos/aula10-Tipo-unknown.ts
@@ -1,10 +1,13 @@
 let x: unknown;
+// let x: any; não chega nada
 
 x = 100;
 x = 'Luiz';
 x = 900;
 x = 10;
 const y = 800;
+
+// console.log(x + y);
 
 if (typeof x === 'number') console.log(x + y);
 
