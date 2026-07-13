b1aaa537af107b2bac6efa4e45bd7497fd183fc7
magno v gomes <magnovgomes74@gmail.com>
2026-07-13 18:47:26 -0300

    feat: tipos null e undefined - null representa ausencia intencional de valor e undefined e a falta de inicialização

---
+++ Secao19-TS-TiposBasicos/aula07-Tipo-null-undefined.ts
@@ -0,0 +1,30 @@
+let x;
+if (typeof x === 'undefined') x = 20;
+console.log(x * 2);
+
+export function createPerson(
+  firstName: string,
+  lastName?: string,
+): {
+  firstName: string;
+  lastName?: string;
+} {
+  return {
+    firstName,
+    lastName,
+  };
+}
+
+export function squareOf(x: any): number | null {
+  if (typeof x === 'number') return x * x;
+  return null;
+}
+
+const squareOfTwoString = squareOf('2');
+// const squareOfTwoString = squareOf(2);
+
+if (squareOfTwoString === null) {
+  console.log('Conta inválida');
+} else {
+  console.log(squareOfTwoString * 100);
+}
