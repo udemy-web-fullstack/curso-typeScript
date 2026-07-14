f2a1350ca299674684d6ec26dd1dbb5985526377
Magno V Gomes <magnovgomes74@gmail.com>
2026-07-14 14:26:26 -0300

    feat: add intersection types lecture examples

---
+++ Secao19-TS-TiposBasicos/aula14-Intersection-types.ts
@@ -0,0 +1,42 @@
+// Definindo tipos separados
+type TemNome = { nome: string };
+type TemIdade = { idade: number };
+type TemCargo = { cargo: string };
+
+// Criando o Intersection Type com o operador '&'
+// Aqui, o tipo 'Funcionario' precisa obrigatoriamente ter nome, idade E cargo.
+type Funcionario = TemNome & TemIdade & TemCargo;
+
+// Agora, ao criar o objeto, o TypeScript vai te cobrar todas as propriedades:
+const magno: Funcionario = {
+  nome: 'Magno V Gomes',
+  idade: 30, // Exemplo
+  cargo: 'Desenvolvedor Full Stack'
+};
+
+console.log(magno);
+
+
+/*
+
+type TemNome = { nome: string };
+type TemSobrenome = { sobrenome: string };
+type TemIdade = { idade: number };
+type Pessoa = TemNome & TemSobrenome & TemIdade; // AND
+
+type AB = 'A' | 'B';
+type AC = 'A' | 'C';
+type AD = 'D' | 'A';
+type Intersecao = AB & AC & AD;
+
+const pessoa: Pessoa = {
+  nome: 'Magno',
+  sobrenome: 'V Gomes',
+  idade: 52,
+};
+
+console.log(pessoa);
+
+// Module mode
+export { pessoa };
+*/
