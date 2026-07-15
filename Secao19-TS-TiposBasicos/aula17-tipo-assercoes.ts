20826a697fd99e4372d5fdad65868376f3f6bea7
magno v gomes <magnovgomes74@gmail.com>
2026-07-15 12:26:52 -0300

    feat: adiciona exemplos da aula de assercoes de tipo

--- Secao19-TS-TiposBasicos/aula17-tipo-assercoes.ts
+++ Secao19-TS-TiposBasicos/aula17-tipo-assercoes.ts
@@ -1,21 +1,34 @@
 /* Recomendado */
+
 // Condicional
 const body1 = document.querySelector('body');
-if (body1) body1.style.background = 'red';
+if (body1) body1.style.background = 'blue';
 
 // Type assertion
 const body3 = document.querySelector('body') as HTMLBodyElement;
-body3.style.background = 'red';
+body3.style.background = 'blue';
 
 // HTMLElement
 const input = document.querySelector('.input') as HTMLInputElement;
-input.value = 'Qualquer coisa';
+input.value = 'Aprendendo TypeScript';
 input.focus();
 
+//************************************************************************
+
 /* Não Recomendado */
+
 // Type assertion
 const body4 = (document.querySelector('body') as unknown) as number;
 
 // Non-null assertion (!)
 const body2 = document.querySelector('body')!;
 body2.style.background = 'red';
+
+
+//**************************************************************************
+
+// 1. O TypeScript acha que isso é apenas um 'Element' genérico ou 'null'
+const body5 = document.querySelector('body');
+
+// 2. Com a Asserção (as), nós garantimos que ele é especificamente o HTMLBodyElement
+const body6 = document.querySelector('body') as HTMLBodyElement;
