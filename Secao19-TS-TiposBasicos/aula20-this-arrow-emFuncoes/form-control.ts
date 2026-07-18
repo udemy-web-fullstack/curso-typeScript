ec740fb2be860f839163aa7b330bf5092f8e1847
magno v gomes <magnovgomes74@gmail.com>
2026-07-18 13:52:43 -0300

    feat: concluir Seção 19 - tipos básicos, validação de formulário e uso de this com arrow functions

---
+++ Secao19-TS-TiposBasicos/aula20-this-arrow-emFuncoes/form-control.ts
@@ -0,0 +1,60 @@
+import { isEmail } from 'validator';
+
+const SHOW_ERROR_MESSAGES = 'show-error-message';
+
+const form = document.querySelector('.form') as HTMLFormElement;
+const username = document.querySelector('.username') as HTMLInputElement;
+const email = document.querySelector('.email') as HTMLInputElement;
+const password = document.querySelector('.password') as HTMLInputElement;
+const password2 = document.querySelector('.password2') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+  const target = e.currentTarget as HTMLFormElement;
+  hideErrorMessages(target);
+  checkForEmptyFields(username, email, password, password2);
+  checkEmail(email);
+  checkEqualPasswords(password, password2);
+  if (shouldSendForm(target)) console.log('Formulário enviado com sucesso!');
+});
+
+function checkForEmptyFields(...inputs: HTMLInputElement[]): void {
+  inputs.forEach((input) => {
+    if (!input.value)
+      showErrorMessage(input, 'Preenchimento obrigatório deste campo');
+  });
+};
+
+function checkEmail(input: HTMLInputElement): void {
+  if (!isEmail(input.value)) showErrorMessage(input, 'Email inválido');
+}
+
+function checkEqualPasswords(password: HTMLInputElement, password2: HTMLInputElement) {
+  if (password.value !== password2.value) {
+    showErrorMessage(password, 'As senhas não são iguais.');
+    showErrorMessage(password2, 'As senhas não são iguais.');
+  }
+}
+
+function hideErrorMessages(form: HTMLFormElement): void {
+  form.querySelectorAll('.' + SHOW_ERROR_MESSAGES).forEach((item) => item.classList.remove(SHOW_ERROR_MESSAGES));
+}
+
+function showErrorMessage(input: HTMLInputElement, msg: string): void {
+  const formFields = input.parentElement as HTMLDivElement;
+  const errorMessage = formFields.querySelector('.error-message') as HTMLSpanElement;
+
+  errorMessage.innerText = msg;
+  formFields.classList.add(SHOW_ERROR_MESSAGES);
+}
+
+function shouldSendForm(form: HTMLFormElement): boolean {
+  let send = true;
+  form.querySelectorAll('.' + SHOW_ERROR_MESSAGES).forEach(() => (send = false));
+  return send;
+}
+
+
+
+// showErrorMessage(username, 'MENSAGEM');
+// hideErrorMessages(form);
