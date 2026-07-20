c13a052918bc09713ce9a7b638bff84a7bc9985b
magno v gomes <magnovgomes74@gmail.com>
2026-07-20 20:09:32 -0300

    feat: implementar getters e setters para encapsulamento

---
+++ Secao20-POO-Classes-Interf/aula06-Getters-Setters/index.ts
@@ -0,0 +1,49 @@
+export class Pessoa {
+  constructor(
+    private nome: string,
+    private sobrenome: string,
+    private idade: number,
+    private _cpf: string,
+  ) {
+    this.cpf = _cpf;              // ← ① CHAMA o setter
+  }
+
+  set cpf(cpf: string) {          // ← executa quando alguém ATRIBUI um valor
+    console.log('SETTER foi chamado', '\n');
+    this._cpf = cpf;
+  }
+
+  get cpf(): string {             // ← executa quando alguém LÊ o valor
+    console.log('GETTER tb foi chamado', '\n');
+    return this._cpf.replace(/\D/g, '');
+  }
+}
+
+const pessoa = new Pessoa('Magno', 'V Gomes', 52, '111.222.333-44');
+// ↑ entra no constructor
+// ↑ dentro do constructor chama o setter (①)
+
+pessoa.cpf = '444.333.222-11';    // ← ② CHAMA o setter novamente
+
+console.log(pessoa.cpf, '\n');    // ← ③ CHAMA o getter
+
+
+
+/* ##### COMPORTAMENTO ANTIGO DE GET E SET #####
+  getCpf(): string {
+    return this.cpf.replace(/\D/g, '');
+  }
+  getNome(): string {
+    return this.nome;
+  }
+
+  setCpf(valor: string): void {
+    this.cpf = valor;
+  }
+
+  getCpf(): string {
+    return this.cpf.replace(/\D/g, '');
+  }
+
+console.log(pessoa.getNome(), '\n');
+*/
