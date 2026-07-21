4f898901b066cf6c85da7146dbe29ed3b69c94b5
magno v gomes <magnovgomes74@gmail.com>
2026-07-21 14:18:17 -0300

    feat: implementa atributos e métodos estáticos em TypeScript

--- Secao20-POO-Classes-Interf/aula07-Atributos-metEstaticos/static.ts
+++ Secao20-POO-Classes-Interf/aula07-Atributos-metEstaticos/static.ts
@@ -9,6 +9,10 @@ export class Pessoa {
     public cpf: string,
   ) {}
 
+  metodoNormal(): void {
+    console.log(Pessoa.idadePadrao, Pessoa.cpfUnico, '\n');
+  }
+
   static novaPessoa(nome: string, sobrenome: string): Pessoa {
     return new Pessoa(nome, sobrenome, Pessoa.idadePadrao, Pessoa.cpfUnico);
     // return new Pessoa(nome, sobrenome, 0, '000.000.000-00');
@@ -19,6 +23,8 @@ const pessoa1 = new Pessoa('Magno', 'V Gomes', 52, '111.222.333-44');
 const pessoa2 = Pessoa.novaPessoa('Antonio Gael', 'Vargas');
 console.log(pessoa1, '\n');
 console.log(pessoa2, '\n');
+pessoa2.metodoNormal();
+console.log(Pessoa.idadePadrao, Pessoa.cpfUnico, '\n');
 
 // static agradeca(): void {
 //   console.log('Deus é fiel!!', '\n');
