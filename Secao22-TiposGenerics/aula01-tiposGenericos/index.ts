c8eb898c571e9dd520b9f3d572cde3c1811cfe92
magno v gomes <magnovgomes74@gmail.com>
2026-07-27 17:03:41 -0300

    feat: implementa introdução aos tipos genéricos

--- Secao22-TiposGenerics/aula01-tiposGenericos/index.ts
+++ Secao22-TiposGenerics/aula01-tiposGenericos/index.ts
@@ -1,21 +1,67 @@
-type FilterCallback<U> = (value: U, index?: number, array?: U[]) => boolean;
+// ****** AQUI com types generics ******
 
-export function myFilter<T>(array: T[], callbackfn: FilterCallback<T>): T[] {
-  const newArray = [];
+type FiltroCallback<U> = (
+  value: U,
+  // value: unknown, unknown[], AQUI entra o 'U' de tipo
+  index?: number,
+  array?: U[],
+  // array?: unknown[], unknown[], AQUI entra o 'U' de tipo
+) => boolean;
+
+export function MeuFiltro<T>(
+  array: T[],
+  // array: unknown[], AQUI entra o 'T' de tipo
+  callbackfn: FiltroCallback<T>,
+): T[] {
+  const novoArray = [];
 
   for (let i = 0; i < array.length; i++) {
     if (callbackfn(array[i])) {
-      newArray.push(array[i]);
+      novoArray.push(array[i]);
     }
   }
-
-  return newArray;
+  return novoArray;
 }
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const arrayFilterOrigin = array.filter((value) => value < 5);
-console.log(arrayFilterOrigin);
+const arrayFiltradoOriginal = array.filter((value) => value <= 5);
+console.log(arrayFiltradoOriginal);
 
-const arrayMyFilter = myFilter(array, (value) => value < 5);
-console.log(arrayMyFilter);
+const arrayFitrado = MeuFiltro(array, (value) => value <= 5);
+console.log(arrayFitrado);
+
+// ****** AQUI sem types generics ******
+
+/*
+type FiltroCallback = (
+  value: unknown,
+  index?: number,
+  array?: unknown[],
+) => boolean;
+
+export function MeuFiltro(
+  array: unknown[],
+  callbackfn: FiltroCallback,
+): unknown[] {
+  const novoArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (callbackfn(array[i])) {
+      novoArray.push(array[i]);
+    }
+  }
+  return novoArray;
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const arrayFiltradoOriginal = array.filter((value) => value <= 5);
+console.log(arrayFiltradoOriginal);
+
+const arrayFitrado = MeuFiltro(array, (value) => {
+  if (typeof value === 'number') return value <= 5;
+  return false;
+});
+console.log(arrayFitrado);
+*/
